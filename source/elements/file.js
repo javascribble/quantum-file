0c4dc43ecf443b9517574b2b0735878eb0ff401d
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2021-03-04 10:07:40 -0500

    merge repositories

--- source/elements/file.js
+++ source/elements/file.js
@@ -1,9 +1,5 @@
-import html from '../templates/file.js';
-
 export class File extends Quantum {
     constructor() {
         super();
     }
 }
-
-File.define('quantum-file', html);
--- source/main.js
+++ source/main.js
@@ -1,1 +1,4 @@
-import './elements/file.js';
+import { File } from './elements/file.js';
+import file from './templates/file.js';
+
+File.define('quantum-file', file);
